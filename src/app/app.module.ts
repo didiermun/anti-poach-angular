e9bc62aea040277e835a18e1e37f76f22ccf30a4
didiermunezero <didiermunezer38@gmail.com>
2021-05-22 23:16:55 +0200

    base files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { ReportCardComponent } from './report-card/report-card.component';
+import { HomeComponent } from './home/home.component';
+import { ReportViewComponent } from './report-view/report-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ReportCardComponent,
+    HomeComponent,
+    ReportViewComponent
   ],
   imports: [
     BrowserModule,
