5b03b0838e5ce3d5dc62740ebce581d3b4b7718b
didiermunezero <didiermunezer38@gmail.com>
2021-05-25 22:12:33 +0200

    feat: update form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
+  formControl = new FormControl('', [
+    Validators.required
+    // Validators.email,
+  ]);
+  data: string | undefined;
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? 'Required field' :
+      this.formControl.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+  public confirmAdd(): void {
+    // console.log(this.data);
+  }
+  submit() {
+  }
   ngOnInit(): void {
   }
 
