4ee6cc468606c3ddf6208dd582ad85e720fe392c
didiermunezero <didiermunezer38@gmail.com>
2021-05-25 04:11:56 +0200

    feat: form checker

--- src/app/dialogs/new-record/new-record.component.ts
+++ src/app/dialogs/new-record/new-record.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-new-record',
@@ -7,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewRecordComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<NewRecordComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
+    formControl = new FormControl('', [
+      Validators.required
+      // Validators.email,
+    ]);
+  
+    getErrorMessage() {
+      return this.formControl.hasError('required') ? 'Required field' :
+        this.formControl.hasError('email') ? 'Not a valid email' :
+          '';
+    }
+  
+    submit() {
+    // emppty stuff
+    }
+  
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+  
+    public confirmAdd(): void {
+      console.log(this.data);
+    }
   ngOnInit(): void {
   }
 
