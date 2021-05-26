2444c644e901f5282db1452cd2962d3ae5dd645c
didiermunezero <didiermunezer38@gmail.com>
2021-05-26 22:29:44 +0200

    feat: populate data table

--- src/app/patrouille/patrouille.component.ts
+++ src/app/patrouille/patrouille.component.ts
@@ -7,6 +7,15 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { QueryRef,Apollo, gql } from 'apollo-angular';
 import {NewRecordComponent} from '../dialogs/new-record/new-record.component';
 import {LoggedinService} from '../services/loggedin/loggedin.service';
+import {MatSort} from '@angular/material/sort';
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+
 
 export interface DialogData {
   animal: string;
@@ -45,6 +54,38 @@ const GET_PATROUILLES = gql`
     }
   }
 `;
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+
+export interface Record{
+  id: string;
+  northing: number;
+  easting: number;
+  wpt: number;
+  observation: string;
+  otype: string;
+  remarks: string[];
+  number: number;
+  sitename: string;
+}
+export interface pat{
+  patrouille: any;
+  records: Record[];
+}
+
+/** Constants used to fill up our data base. */
+const COLORS: string[] = [
+  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+  'aqua', 'blue', 'navy', 'black', 'gray'
+];
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
 @Component({
   selector: 'app-patrouille',
   templateUrl: './patrouille.component.html',
@@ -54,17 +95,25 @@ const GET_PATROUILLES = gql`
 export class PatrouilleComponent implements OnDestroy,OnInit,AfterViewInit  {
 
  
-  constructor(private logged: LoggedinService,public dialog: MatDialog,private route: ActivatedRoute,private apollo: Apollo) { }
+  constructor(private logged: LoggedinService,public dialog: MatDialog,private route: ActivatedRoute,private apollo: Apollo) {
+     // Create 100 users
+    //  const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+     // Assign the data to the data source for the table to render
+    //  this.dataSource = new MatTableDataSource(this.patrouille.records);
+   }
   loading: boolean = true;
-  patrouille: any = {patrouille:{},records:[]};
+  patrouille: pat = {patrouille:{},records:[]};
   patrouilleQuery!: QueryRef<any>;
   subscription: any;
   isloggedIn: boolean = false;
   private querySubscription!: Subscription;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   
+  displayedColumns: string[] = ['time', 'wpt','location','number','observation','otype','sitename'];
+  dataSource!: MatTableDataSource<Record>;
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   addNew() {
     console.log(this.patrouille.patrouille.id)
     const dialogRef = this.dialog.open(NewRecordComponent, {
@@ -91,8 +140,11 @@ export class PatrouilleComponent implements OnDestroy,OnInit,AfterViewInit  {
         this.querySubscription = this.patrouilleQuery
       .valueChanges
       .subscribe(({ data, loading }) => {
-        this.loading = loading;
         this.patrouille = data.patrouille;
+        this.dataSource = new MatTableDataSource(this.patrouille.records);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+        this.loading = loading;
     });
       }
     );
@@ -109,11 +161,30 @@ export class PatrouilleComponent implements OnDestroy,OnInit,AfterViewInit  {
     this.querySubscription.unsubscribe();
   }
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+   
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
+}
 export interface PeriodicElement {
   name: string;
   position: number;
