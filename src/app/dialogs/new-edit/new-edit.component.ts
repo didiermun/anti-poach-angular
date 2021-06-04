b61fbc8c448c21de5c7aeb02698b33a9b854b403
didiermunezero <didiermunezer38@gmail.com>
2021-06-04 16:05:29 +0200

    feat: add new token

--- src/app/dialogs/new-edit/new-edit.component.ts
+++ src/app/dialogs/new-edit/new-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {FormControl, Validators} from '@angular/forms';
 import { gql,Apollo } from 'apollo-angular';
+import { NotifierService } from 'angular-notifier';
 
 const NEW_CODE = gql`
   mutation newCode($data: NewCode!) {
@@ -17,9 +18,12 @@ const NEW_CODE = gql`
   styleUrls: ['./new-edit.component.css']
 })
 export class NewEditComponent implements OnInit {
+  private readonly notifier: NotifierService;
 
-  constructor(private apollo: Apollo,public dialogRef: MatDialogRef<NewEditComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(notifierService: NotifierService,private apollo: Apollo,public dialogRef: MatDialogRef<NewEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+      this.notifier = notifierService;
+     }
 
     formControl = new FormControl('', [
       Validators.required
@@ -46,6 +50,7 @@ export class NewEditComponent implements OnInit {
         data: this.data
       }
     }).subscribe(({ data }) => {
+      this.notifier.notify('success','Code added successfully!')
       console.log('got data', data);
     },(error) => {
       console.log('there was an error sending the query', error);
